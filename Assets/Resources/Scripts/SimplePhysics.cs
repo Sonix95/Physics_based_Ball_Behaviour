4d0f1d383f8cba517a92ee050f7709ff12917474
Сергей Маслёнченко <44851397+Sonix95@users.noreply.github.com>
2019-01-18 19:39:59 +0300

    Add Spawn Manager and Clean Code

---
+++ Assets/Resources/Scripts/SimplePhysics.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class SimplePhysics : MonoBehaviour
+{
+    #region Fields
+
+    public float gravity = 10f;
+
+    Transform platform;
+
+    private float _currentYCoord = 0;
+    public float CurrentYCoord
+    {
+        get => _currentYCoord;
+        set {
+            if (value < platform.transform.position.y - 50 || value > platform.transform.position.y + 150)
+            {
+                Destroy(gameObject);
+            }
+            else
+                _currentYCoord = value;
+        }
+    }
+
+    private float _startYCoord = 0;
+
+    public float startSpeed = 0;
+    private float _currentSpeed = 0;
+
+    private float _startTime = 0;
+    private float _currentTime = 0;
+
+    #endregion
+
+    #region Methods
+
+    void Start()
+    {
+        platform = GameObject.FindWithTag("Plane").transform;
+        _startYCoord = transform.position.y;
+        _startTime = Time.time;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        startSpeed = -_currentSpeed;
+        _startYCoord = transform.position.y;
+        _startTime = Time.time;
+    }
+
+    private void ChangeCoordinate()
+    {
+        Vector3 pos = transform.position;
+
+        _currentTime = Time.time - _startTime;
+        _currentSpeed = startSpeed - gravity * _currentTime;
+
+        CurrentYCoord = _startYCoord + startSpeed * _currentTime - (gravity * _currentTime * _currentTime) / 2;
+
+        pos.y = CurrentYCoord;
+        transform.position = pos;
+    }
+
+    void FixedUpdate()
+    {
+        ChangeCoordinate();
+    }
+
+    #endregion
+}
---
+++ Assets/Resources/Scripts/SpawnManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    #region Fields
+
+    [Header("Primitive Settings")]
+    public PrimitiveType buildinPrimitive = PrimitiveType.Sphere;
+    public float gravity = 9.81f;
+    public float startSpeed = 0f;
+
+    [Header("Random Primitive Settings")]
+    public bool useRandomGravity = false;
+    public bool useRandomPrimitive = false;
+    public bool useRandomStartSpeed = false;    
+
+    [Header("Invoke Time")]
+    public float startCall = 2f;
+    public float repeatTime = 2f;
+
+    [Header("X coordinates")]
+    public float xMin = -20f;
+    public float xMax = 20f;
+
+    [Header("Y coordinates")]
+    public float yMin = 5f;
+    public float yMax = 20f;
+
+    #endregion
+
+    #region Methods
+
+    private void OnEnable()
+    {
+        StartCoroutine(instObj());
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(instObj());
+    }
+
+    IEnumerator instObj()
+    {
+        GeneratePrimitive();
+        yield return new WaitForSeconds(repeatTime);
+        StartCoroutine(instObj());
+    }
+
+    private PrimitiveType GetRandomPrimitive()
+    {
+        int randomPrimitive = Random.Range(0, 4);
+        PrimitiveType primitive = PrimitiveType.Sphere;
+
+        switch(randomPrimitive)
+        {
+            case 0:
+                primitive = PrimitiveType.Sphere;
+                break;
+            case 1:
+                primitive = PrimitiveType.Capsule;
+                break;
+            case 2:
+                primitive = PrimitiveType.Cylinder;
+                break;
+            case 3:
+                primitive = PrimitiveType.Cube;
+                break;
+        }
+
+        return primitive;
+    }
+
+    private void GeneratePrimitive()
+    {
+        Vector3 position = Vector3.zero;
+        position.x = Random.Range(xMin, xMax);
+        position.y = Random.Range(yMin, yMax);
+
+        GameObject primitiveObject = GameObject.CreatePrimitive(useRandomPrimitive ? GetRandomPrimitive() : buildinPrimitive);
+        primitiveObject.transform.position = position;
+        primitiveObject.AddComponent<Rigidbody>().useGravity = false;
+        primitiveObject.AddComponent<SimplePhysics>().gravity = useRandomGravity ? Random.Range(-10,10) : gravity;
+        primitiveObject.GetComponent<SimplePhysics>().startSpeed = useRandomStartSpeed ? Random.Range(-20, 20) : startSpeed;
+    }
+
+    #endregion
+}
